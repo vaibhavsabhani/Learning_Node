fed95900827b30eaa917a6fb920fe2705ab29580
vaibhav Sabhani <vvsabhani1@gmail.com>
2025-07-20 11:21:19 +0530

    add new File

---
+++ xyz.js
@@ -0,0 +1,14 @@
+// all the code of the module inside a function(IIFE)
+// IIFE -Immediately Invoked Function Expression
+
+( function (require) {
+
+    // require('path'); // if you need to use path module, uncomment this line
+     
+    //all the code funtion run here 
+
+    var name = "Vaibhav sabhani";
+
+})();
+
+var name = "Vaibhav Patel";  // this will not affect the name inside the IIFE   
